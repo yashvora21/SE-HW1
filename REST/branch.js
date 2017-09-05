3647625abf2c4a1658a27f8e96dd400212029d54
yvora <yvora@ncsu.edu>
2017-09-05 14:49:19 -0400

    Update branch.js

--- REST/branch.js
+++ REST/branch.js
@@ -8,8 +8,8 @@ const rl = readline.createInterface({
 	output: process.stdout
       });
 
-var token = "token " + "aed91c8d10d8e9b43bb204d07d91d4122d1b13de";
-var userId = "yashvora21";
+var token = "token " + "<your-token-here>";
+var userId = "<your-username-here>";
 
 var urlRoot = "https://api.github.com";
 // NCSU Enterprise endpoint:
